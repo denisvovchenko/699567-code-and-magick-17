1ad5d5a87b154828f28335a22f4dbee2e3941793
Denys <denisvovchenko96@mail.ru>
2019-06-08 11:58:00 +0200

    refÑactoring (not complete)

--- js/stat.js
+++ js/stat.js
@@ -1,64 +1,134 @@
 'use strict';
 
-var STAT_WIDTH = 420;
-var STAT_HEIGHT = 270;
+var STAT = {
+  dimensions: {
+    x: 100,
+    y: 10,
+    width: 420,
+    height: 270,
+  },
+
+  background: '#ffffff',
+};
+
+STAT.shadow = {
+  dimensions: {
+    x: STAT.dimensions.x + 10,
+    y: STAT.dimensions.y + 10,
+    width: STAT.dimensions.width,
+    height: STAT.dimensions.height,
+  },
+
+  background: 'rgba(0, 0, 0, 0.7)',
+};
+
+STAT.textDimensions = {
+  x: STAT.dimensions.x + 20,
+  y: STAT.dimensions.y + 40,
+};
+
+var TEXT_STYLES = {
+  font: '16px PT Mono',
+  lineHeight: 20,
+  color: '#000000',
+};
+
+var SUCCESS_TEXT_STRINGS = ['Ура вы победили!', 'Список результатов:'];
+
+var GIST = {
+  dimensions: {
+    x: 140,
+    y: STAT.dimensions.y + 115,
+    height: 150,
+  },
+
+  gap: 50,
+  timeMarginBottom: 10,
+  nameMarginTop: 20,
+};
+
+GIST.columnDimensions = {
+  x: GIST.dimensions.x,
+  width: 40,
+};
 
 window.renderStatistics = function (ctx, names, times) {
-  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-  ctx.fillRect(110, 20, STAT_WIDTH, STAT_HEIGHT);
+  paintStatWindow(ctx);
 
-  ctx.fillStyle = '#ffffff';
-  ctx.fillRect(100, 10, STAT_WIDTH, STAT_HEIGHT);
+  writeSuccessText(ctx);
 
-  ctx.fillStyle = '#000000';
-  ctx.font = '16px PT Mono';
+  var highestTime = getHighestTime(times);
 
-  var successTextStrings = ['Ура вы победили!', 'Список результатов:'];
-  var lineHeight = 20;
-  var textCoordsX = 120;
-  var textCoordY = 40;
+  for (var i = 0; i < names.length; i++) {
+    var columnProps = {
+      name: names[i],
+      time: Math.round(times[i]),
+      highestTime: highestTime,
+    };
 
-  for (var i = 0; i < successTextStrings.length; i++, textCoordY += lineHeight) {
-    ctx.fillText(successTextStrings[i], textCoordsX, textCoordY);
-  }
+    paintColumn(ctx, columnProps);
 
-  var gistHeight = 150;
-  var gistMarginTop = 15;
-  var gistCoordY = textCoordY + gistMarginTop;
-  var gistGap = 50;
-
-  var timeMarginBottom = 10;
-  var nameMarginTop = 20;
-
-  var columnCoordX = 140;
-  var columnWidth = 40;
-  var highestColumn = times[0];
-  var nextTime;
-
-  for (i = 0; i < names.length - 1; i++) {
-    nextTime = Math.round(times[i + 1]);
-    highestColumn = (highestColumn > nextTime) ? highestColumn : nextTime;
-  }
-
-  for (i = 0; i < names.length; i++, columnCoordX += columnWidth + gistGap) {
-    var time = Math.round(times[i]);
-    var name = names[i];
-
-    var columnHeight = Math.round(gistHeight / highestColumn * time);
-    var columnCoordY = gistCoordY + gistHeight - columnHeight;
-    var columnSaturation = Math.random() * 100 + '%';
-
-    var myColor = 'rgba(255, 0, 0, 1)';
-    var otherPlayerColor = 'hsl(240, ' + columnSaturation + ', 50%)';
-
-    ctx.fillStyle = (name === 'Вы') ? myColor : otherPlayerColor;
-    ctx.fillRect(columnCoordX, columnCoordY, columnWidth, columnHeight);
-
-    var timeCoordY = columnCoordY - timeMarginBottom;
-    var nameCoordY = gistCoordY + gistHeight + nameMarginTop;
-
-    ctx.fillStyle = '#000000';
-    ctx.fillText(time, columnCoordX, timeCoordY);
-    ctx.fillText(name, columnCoordX, nameCoordY);
+    GIST.columnDimensions.x += GIST.columnDimensions.width + GIST.gap;
   }
 };
+
+function paintStatWindow(ctx) {
+  ctx.fillStyle = STAT.shadow.background;
+  ctx.fillRect(STAT.shadow.dimensions.x, STAT.shadow.dimensions.y, STAT.shadow.dimensions.width, STAT.shadow.dimensions.height);
+
+  ctx.fillStyle = STAT.background;
+  ctx.fillRect(STAT.dimensions.x, STAT.dimensions.y, STAT.dimensions.width, STAT.dimensions.height);
+}
+
+function writeSuccessText(ctx) {
+  ctx.fillStyle = TEXT_STYLES.color;
+  ctx.font = TEXT_STYLES.font;
+
+  for (var i = 0; i < SUCCESS_TEXT_STRINGS.length; i++, STAT.textDimensions.y += TEXT_STYLES.lineHeight) {
+    ctx.fillText(SUCCESS_TEXT_STRINGS[i], STAT.textDimensions.x, STAT.textDimensions.y);
+  }
+}
+
+function getHighestTime(times) {
+  var highestTime = 0;
+
+  for (var i = 0; i < times.length; i++) {
+    var currentTime = Math.round(times[i]);
+
+    highestTime = (highestTime > currentTime) ? highestTime : currentTime;
+  }
+
+  return highestTime;
+}
+
+function getColumnColor(name) {
+  var myColor = 'rgba(255, 0, 0, 1)';
+  var columnSaturation = Math.random() * 100 + '%';
+  var otherPlayerColor = 'hsl(240, ' + columnSaturation + ', 50%)';
+
+  return (name === 'Вы') ? myColor : otherPlayerColor;
+}
+
+function setColumnVerticalDimensions(highestTime, time) {
+  GIST.columnDimensions.height = Math.round(GIST.dimensions.height / highestTime * time);
+  GIST.columnDimensions.y = GIST.dimensions.y + GIST.dimensions.height - GIST.columnDimensions.height;
+}
+
+function paintColumn(ctx, props) {
+  var time = props.time;
+  var name = props.name;
+  var highestTime = props.highestTime;
+
+  ctx.fillStyle = getColumnColor(name);
+
+  setColumnVerticalDimensions(highestTime, time);
+
+  ctx.fillRect(GIST.columnDimensions.x, GIST.columnDimensions.y, GIST.columnDimensions.width, GIST.columnDimensions.height);
+
+  var timeCoordY = GIST.columnDimensions.y - GIST.timeMarginBottom;
+  var nameCoordY = GIST.dimensions.y + GIST.dimensions.height + GIST.nameMarginTop;
+
+  ctx.fillStyle = TEXT_STYLES.color;
+  ctx.fillText(time, GIST.columnDimensions.x, timeCoordY);
+  ctx.fillText(name, GIST.columnDimensions.x, nameCoordY);
+}
